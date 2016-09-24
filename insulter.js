0ccd011e8877e80cb2e7c4a126aa6626060db449
Adam Moran <amoran@cmu.edu>
2016-09-24 18:41:41 -0500

    add initial custom insults

--- insulter.js
+++ insulter.js
@@ -1,6 +1,25 @@
 
+var genericInsults = [
+	'Fuck you <name>, you stupid sack of siberian sheep shit.',
+  '<name>, is your ass jealous of the amount of shit that just came out of your mouth?',
+  'I hope you realize everyones just putting up with you, <name>.'
+];
+
+var measuresInsults = [
+	'I should have pulled out and shot <name> on the wall!'
+];
+
 function insult(name) {
-	return '' + name + ' sucks!';
+	var insult;
+	if (['ian', 'Ian', 'Isaac', 'isaac'].indexOf(name) > -1) {
+		insult = measuresInsults[Math.floor(Math.random()*measuresInsults.length)];
+		return insult.replace('<name>', name);
+	} else if (['adam', 'Adam', 'amoran', 'Adumb', 'adumb'].indexOf(name) > -1) {
+		return 'I will not insult my master!';
+	} else {
+		insult = genericInsults[Math.floor(Math.random()*genericInsults.length)];
+		return insult.replace('<name>', name);
+	}
 }
 
 
